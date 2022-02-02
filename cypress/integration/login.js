ac6b65caa2586eedfab5904e71a3f16719f6d08a
gururajhm1977 <95733239+gururajhm1977@users.noreply.github.com>
2022-02-02 10:07:39 +0530

    added tags tests

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,41 +1,45 @@
 import { login } from '../fixtures/selectors.json'
 import { Utility } from "../support/utility"
+import TestFilters from '../support/filterTests.js'
 
 //Call getBaseUrl() to get environment specific url value
-const url = new Utility().getBaseUrl();
 
-describe('login page', function () {
-    it('login to site...', () => {
-        cy.visit(url);
+TestFilters(['smoke', 'regression'], () => {
+    const url = new Utility().getBaseUrl();
 
-        cy.get(login.username).type(Cypress.env("username"));
-        cy.get(login.password).type(Cypress.env("password"));
+    describe('login page', function () {
+        it('login to site...', () => {
+            cy.visit(url);
+
+            cy.get(login.username).type(Cypress.env("username"));
+            cy.get(login.password).type(Cypress.env("password"));
 
 
-        cy.get(login.button).click()
-        cy.wait(5000);
-        const customThresholds = {
-            performance: 10,
-            accessibility: 50,
-            seo: 70,
-            // 'first-contentful-paint': 4000,
-            // 'largest-contentful-paint': 4000,
-            // 'cumulative-layout-shift': 0.1,
-            // 'total-blocking-time': 500,
-        };
+            cy.get(login.button).click()
+            cy.wait(5000);
+            const customThresholds = {
+                performance: 10,
+                accessibility: 50,
+                seo: 70,
+                // 'first-contentful-paint': 4000,
+                // 'largest-contentful-paint': 4000,
+                // 'cumulative-layout-shift': 0.1,
+                // 'total-blocking-time': 500,
+            };
 
-        const desktopConfig = {
-            formFactor: 'desktop',
-            screenEmulation: { disabled: true },
-        };
+            const desktopConfig = {
+                formFactor: 'desktop',
+                screenEmulation: { disabled: true },
+            };
 
-        //  cy.lighthouse(customThresholds, desktopConfig);
-        cy.wait(5000);
-        cy.get(login.salesmenu).click();
+            //  cy.lighthouse(customThresholds, desktopConfig);
+            cy.wait(5000);
+            cy.get(login.salesmenu).click();
 
-        cy.get(login.orders).click();
+            cy.get(login.orders).click();
 
-        cy.get(login.logout).click();
+            cy.get(login.logout).click();
 
-    })
+        })
+    });
 });
--- cypress/integration/monkeyTesting.js
+++ cypress/integration/monkeyTesting.js
@@ -1,27 +1,32 @@
 import { createHorde, species, strategies } from 'gremlins.js';
 import Chance from 'chance';
 //import { createHorde, species, strategies } from '../../src/index';
+import TestFilters from '../support/filterTests.js'
+
+
 const seed = 'formFiller';
+TestFilters(['regression'], () => {
 
-describe('Run gremlins.js inside a cypress test', () => {
-    let horde;
-    beforeEach(() => {
-        return cy.visit('https://www.google.com').then(() =>
-            cy.window().then((pageWindow) => {
-                horde = createHorde({
+    describe('Run gremlins.js inside a cypress test', () => {
+        let horde;
+        beforeEach(() => {
+            return cy.visit('https://www.google.com').then(() =>
+                cy.window().then((pageWindow) => {
+                    horde = createHorde({
 
-                    species: [species.formFiller()],
-                    strategies: [strategies.bySpecies({ nb: 10 })],
-                    window: pageWindow,
-                    randomizer: new Chance(seed),
-                });
-            })
-        );
-    });
+                        species: [species.formFiller()],
+                        strategies: [strategies.bySpecies({ nb: 10 })],
+                        window: pageWindow,
+                        randomizer: new Chance(seed),
+                    });
+                })
+            );
+        });
 
-    it('should run gremlins.js', () => {
-        return cy.wrap(horde.unleash()).then(() => {
-            /* ... */
+        it('should run gremlins.js', () => {
+            return cy.wrap(horde.unleash()).then(() => {
+                /* ... */
+            });
         });
     });
 });
